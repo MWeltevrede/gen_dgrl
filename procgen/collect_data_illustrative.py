8e3885dcf2fb7033896e616a3be5f06120159f4c
Max Weltevrede <maxweltevrede@gmail.com>
2024-08-14 14:20:48 +0200

    Increase arm length of illustrative environment and add possibility of training and testing with random colours

--- procgen/collect_data_illustrative.py
+++ procgen/collect_data_illustrative.py
@@ -3,14 +3,23 @@ import gym
 import numpy as np
 import io
 
-train_tasks = [((255,0,128), 'left'), ((255,0,128), 'right'), ((255,0,128), 'top'),
-                ((128,255,0), 'left'), ((128,255,0), 'right'), ((128,255,0), 'top'),
-                ((0,128,255), 'left'), ((0,128,255), 'right'), ((0,128,255), 'top')]
+# alternating subgroup
+train_tasks = [((255,0,128), 'left'), ((255,0,128), 'right'), ((255,0,128), 'top'), ((255,0,128), 'bottom'),
+                ((0,255,128), 'left'), ((0,255,128), 'right'), ((0,255,128), 'top'), ((0,255,128), 'bottom')]
 
-test_tasks = [((0,255,128), 'left'), ((0,255,128), 'right'), ((0,255,128), 'top'),
-                ((255,128,0), 'left'), ((255,128,0), 'right'), ((255,128,0), 'top'),
-                ((128,0,255), 'left'), ((128,0,255), 'right'), ((128,0,255), 'top')]
+test_tasks = [((128,255,0), 'left'), ((128,255,0), 'right'), ((128,255,0), 'top'), ((128,255,0), 'bottom'),
+                ((255,128,0), 'left'), ((255,128,0), 'right'), ((255,128,0), 'top'), ((255,128,0), 'bottom'),
+                ((128,0,255), 'left'), ((128,0,255), 'right'), ((128,0,255), 'top'), ((128,0,255), 'bottom'),
+                ((0,128,255), 'left'), ((0,128,255), 'right'), ((0,128,255), 'top'), ((0,128,255), 'bottom')]
 
+# # Random Colours
+# train_tasks = [((206,16,220), 'left'), ((206,16,220), 'right'), ((206,16,220), 'top'), ((206,16,220), 'bottom'),
+#                 ((86,185,105), 'left'), ((86,185,105), 'right'), ((86,185,105), 'top'), ((86,185,105), 'bottom')]
+
+# test_tasks = [((237,48,217), 'left'), ((237,48,217), 'right'), ((237,48,217), 'top'), ((237,48,217), 'bottom'),
+#                 ((52,128,100), 'left'), ((52,128,100), 'right'), ((52,128,100), 'top'), ((52,128,100), 'bottom'),
+#                 ((35,21,88), 'left'), ((35,21,88), 'right'), ((35,21,88), 'top'), ((35,21,88), 'bottom'),
+#                 ((213,109,113), 'left'), ((213,109,113), 'right'), ((213,109,113), 'top'), ((213,109,113), 'bottom')]
 
 register(
      id="IllustrativeCMDPContinuous-v0",
@@ -34,11 +43,11 @@ def optimal_policy(env):
         # The agent is on the top
         return 1
     if agent_location[1] > env.arm_length:
-        # The agent is on the top
+        # The agent is on the bottom
         return 3
 
 
-env = gym.make('IllustrativeCMDPContinuous-v0', tasks=train_tasks)
+env = gym.make('IllustrativeCMDPContinuous-v0', tasks=train_tasks, arm_length=6)
 env.seed(88)
 obs = env.reset()
 
@@ -46,16 +55,18 @@ ep_obs = [obs]
 ep_rewards = []
 ep_dones = []
 ep_actions = []
-for i in range(6):
-     action = optimal_policy(env)
-     obs, reward, done, _ = env.step(action)
-     ep_obs.append(obs)
-     ep_rewards.append([reward])
-     ep_dones.append(done)
-     ep_actions.append([action])
+for i in range(len(train_tasks)):
+    done = False
+    while not done:
+        action = optimal_policy(env)
+        obs, reward, done, _ = env.step(np.array(action))
+        ep_obs.append(obs)
+        ep_rewards.append([reward])
+        ep_dones.append(done)
+        ep_actions.append([action])
 
-     print(reward, done)
-     if done:
+        if done:
+            print(ep_rewards, ep_dones)
             episode = {'observations': np.array(ep_obs), 'actions': np.array(ep_actions), 'rewards': np.array(ep_rewards), 'dones': np.array(ep_dones)}
             with io.BytesIO() as bs:
                 np.savez_compressed(bs, **episode)
